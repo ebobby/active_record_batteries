602c9377650740cc3df64fab085a4cf27f41758a
Francisco Soto <bobby@hellocode.mx>
2015-04-16 15:42:14 -0700

    *BUGFIX* Missing name.

--- lib/hello_code/active_record_utils/concerns/relationship_scopes.rb
+++ lib/hello_code/active_record_utils/concerns/relationship_scopes.rb
@@ -11,11 +11,11 @@ module HelloCode
             with_scope_name    = "with_#{ suffix }".to_sym
             include_scope_name = "include_#{ suffix }".to_sym
             and_scope_name     = "and_#{ suffix }".to_sym
+            load_scope_name    = "load_#{ suffix }".to_sym
 
             scope with_scope_name,    -> { joins(relationship) }
             scope include_scope_name, -> { includes(relationship) }
             scope and_scope_name,     -> { joins(relationship) }
-
             scope load_scope_name,    -> { preload(relationship) }
 
             nil
